9860cb64b3bdc7f0daed1171e60bd568f8431af3
delameter <0.delameter@gmail.com>
2022-12-18 05:03:05 +0300

    [es7s/core][bd86a9b2] 🌱 NEW:  `daemon` restart by command, `monitor` configs reloading

--- es7s/shared/weather_icons.py
+++ es7s/shared/weather_icons.py
@@ -1,6 +1,9 @@
 import datetime
 from dataclasses import dataclass
+from time import sleep
+
 import pytermor as pt
+from pytermor import wait_key
 
 WWO_CODE = {
     "113": "Sunny",
@@ -68,6 +71,10 @@ class DynamicIcon:
             return self.night_icon
         return self.day_icon
 
+    def get_raw(self) -> tuple[str, ...]:
+        return self.day_icon, self.night_icon, self.extra_icon
+
+
 
 class WeatherIconSet:
     NO_COLOR_SET_IDS = [0]
@@ -91,20 +98,28 @@ class WeatherIconSet:
             return icon, pt.NOOP_STYLE
         return icon, self._style
 
+    def get_raw(self, set_id: int) -> str|tuple[str]:
+        if set_id >= len(self._icons):
+            raise IndexError(f"Set #{set_id} is undefined")
+        icon = self._icons[set_id]
+        if isinstance(icon, DynamicIcon):
+            return icon.get_raw()
+        return icon
+
 
 # fmt: off
 WEATHER_ICON_SETS: dict[str, WeatherIconSet] = {  # @FIXME compensate various width
-     "✨": WeatherIconSet(248,	"✨️ ",	"",	"",	"",	("",	"",	""),	wwo_codes=["Unknown"]),
-    "☀": WeatherIconSet(248,	"☀️ ",	"滛",	"滛",	"",	("",	"望",	""),	wwo_codes=["Sunny"]),
-     "☁": WeatherIconSet(248,	"☁️ ",	"摒",	"摒",	"",	("",	""),			wwo_codes=["Cloudy", "VeryCloudy"]),
-    "⛅": WeatherIconSet(248,	"⛅️ ",	"杖",	"杖",	"",	("",	""), 			wwo_codes=["PartlyCloudy"]),
-    "🌫": WeatherIconSet(248,	"🌫️ ",	"敖",	"敖",	"",	("",	""), 			wwo_codes=["Fog"]),
-    "🌦": WeatherIconSet( 27,	"🌦️ ",	"",	"殺",	"",	("",	""), 			wwo_codes=["LightRain", "LightShowers"]),
-    "🌧": WeatherIconSet( 27,	"🌧️ ",	"",	"殺",	"",	("",	""), 			wwo_codes=["HeavyRain", "HeavyShowers", "LightSleet", "LightSleetShowers"]),
-    "⛈": WeatherIconSet(229,	"⛈️ ",	"",	"ﭼ",	"",	("",	""), 			wwo_codes=["ThunderyShowers", "ThunderySnowShowers"]),
-    "🌩": WeatherIconSet(229,	"🌩️ ",	"",	"朗",	"",	("",	""),			wwo_codes=["ThunderyHeavyRain"]),
-    "🌨": WeatherIconSet(153,	"🌨️ ",	"ﰕ",	"流",	"",	("",	""),			wwo_codes=["LightSnow", "LightSnowShowers"]),
-     "❄": WeatherIconSet(153,	"❄️ ",	"",	"",	"",	("",	""),			wwo_codes=["HeavySnow", "HeavySnowShowers"]),
+     "✨": WeatherIconSet(248,	"✨️",	"",	"",	"",	("",	"",	""),	wwo_codes=["Unknown"]),
+    "☀": WeatherIconSet(248,	"☀️",	"滛",	"滛",	"",	("",	"望",	""),	wwo_codes=["Sunny"]),
+     "☁": WeatherIconSet(248,	"☁️",	"摒",	"摒",	"",	("",	""),			wwo_codes=["Cloudy", "VeryCloudy"]),
+    "⛅": WeatherIconSet(248,	"⛅️",	"杖",	"杖",	"",	("",	""), 			wwo_codes=["PartlyCloudy"]),
+    "🌫": WeatherIconSet(248,	"🌫️",	"敖",	"敖",	"",	("",	""), 			wwo_codes=["Fog"]),
+    "🌦": WeatherIconSet( 27,	"🌦️",	"",	"殺",	"",	("",	""), 			wwo_codes=["LightRain", "LightShowers"]),
+    "🌧": WeatherIconSet( 27,	"🌧️",	"",	"殺",	"",	("",	""), 			wwo_codes=["HeavyRain", "HeavyShowers", "LightSleet", "LightSleetShowers"]),
+    "⛈": WeatherIconSet(229,	"⛈️",	"",	"ﭼ",	"",	("",	""), 			wwo_codes=["ThunderyShowers", "ThunderySnowShowers"]),
+    "🌩": WeatherIconSet(229,	"🌩️",	"",	"朗",	"",	("",	""),			wwo_codes=["ThunderyHeavyRain"]),
+    "🌨": WeatherIconSet(153,	"🌨️",	"ﰕ",	"流",	"",	("",	""),			wwo_codes=["LightSnow", "LightSnowShowers"]),
+     "❄": WeatherIconSet(153,	"❄️",	"",	"",	"",	("",	""),			wwo_codes=["HeavySnow", "HeavySnowShowers"]),
 }
 
 WIND_DIRECTION = ["↓", "↙", "←", "↖", "↑", "↗", "→", "↘"]
@@ -138,3 +153,25 @@ def format_weather_icon(origin: str, set_id: int = 0) -> tuple[str, pt.Style]:
         if key == origin:
             return icon_set.get_icon(set_id)
     return origin, pt.NOOP_STYLE
+
+
+
+import sys, unicodedata
+
+
+def measure(icon: str):
+    response = ""
+    print("\x1b[32m"+icon+"\x1b[6n", end='\x1b[34m')
+    sleep(.2)
+    while not response.endswith('R'):
+        response += sys.stdin.read(1) #wait_key()
+    print(pt.ansi.make_erase_in_line(2).assemble()+pt.ansi.make_set_cursor_x_abs(1).assemble(), end='\x1b[m')
+    print(f"{ord(icon):6x}" + '| ' + response.split(';')[1].removesuffix('R').strip()+ ' |'+pt.render(icon, pt.Style(fg='green', bg='black'))+'|', end='\n', file=sys.stderr)
+
+def iterate():
+    for weather_icon, weather_icon_set in WEATHER_ICON_SETS.items():
+        for icon_set_id in range(0, 5):
+            icons = weather_icon_set.get_raw(icon_set_id)
+            for icon in icons:
+                if not icon: continue
+                measure(icon)
